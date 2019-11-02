3f26a85567587688a1151434f8636751977adbe8
Ayodeji Ilori <ayojava@gmail.com>
2019-11-02 20:29:47 +0100

    Added flyway DB migration to Test

--- src/test/java/org/javasoft/demoapi/DemoApiApplicationTests.java
+++ src/test/java/org/javasoft/demoapi/DemoApiApplicationTests.java
@@ -1,13 +1,38 @@
 package org.javasoft.demoapi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApiApplication.class , webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApiApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    private HttpHeaders httpHeaders;
+
+    @Before
+    public void init() {
+        httpHeaders = new HttpHeaders();
     }
+
+    private String createUrlWithPort(String uri) {
+        return "http://localhost:" + port  + uri;
+    }
+
+
 
 }
